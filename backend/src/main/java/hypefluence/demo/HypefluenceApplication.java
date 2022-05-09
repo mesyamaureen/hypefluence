9468c5af3e5d6962a9449f7fc7e72688780f4569
s90664 <s90664@bht-berlin.de>
2022-05-09 22:58:34 +0200

    backend added

---
+++ backend/src/main/java/hypefluence/demo/HypefluenceApplication.java
@@ -0,0 +1,13 @@
+package hypefluence.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypefluenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypefluenceApplication.class, args);
+	}
+
+}
