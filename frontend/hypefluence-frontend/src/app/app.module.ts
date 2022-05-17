6d3be3e1dc703426628b654c781261d5c05972e1
s90664 <s90664@bht-berlin.de>
2022-05-17 18:36:16 +0200

    registrieren component erstellt

--- frontend/hypefluence-frontend/src/app/app.module.ts
+++ frontend/hypefluence-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrierenComponent } from './registrieren/registrieren.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrierenComponent
   ],
   imports: [
     BrowserModule,
